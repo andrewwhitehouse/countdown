e1c88cd7f8c6560f5847838b7241c48eec6ba2f7
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-08-12 11:25:24 +0100

    Revised candidates function

--- src/numberly/core.clj
+++ src/numberly/core.clj
@@ -1,5 +1,6 @@
 (ns numberly.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
 
 (defn numbers [small-count large-count]
   (if (not= 6 (+ small-count large-count))
@@ -10,6 +11,55 @@
         small (take small-count (shuffle (repeatedly 20 #(inc (rand-int 10)))))]
     (concat small large)))
 
+(defn sums
+  ([numbers total] (sums numbers total {:matched [] :candidates [[]]}))
+  ([numbers total working]
+   (if-let [number (first numbers)]
+     (let [new-candidates (->> working
+                               :candidates
+                               (mapv #(conj % number))
+                               (group-by #(= total (apply + %))))]
+       (recur
+         (rest numbers)
+         total
+         (-> working
+             (update :matched concat (new-candidates true))
+             (update :candidates concat (new-candidates false)))))
+     (working :matched))))
+
+(defn candidates [numbers]
+  (let [cnt (count numbers)]
+    (distinct
+      (map
+        (fn [{:keys [len start-index]}]
+          (->> numbers (drop start-index) (take len) (sort)))
+        (for [len (range 1 (inc cnt))
+              start-index (range cnt)
+              :when (<= (+ len start-index) cnt)]
+          {:len len :start-index start-index})))))
+
 (defn -main [& args]
   (println "Numbers" (str/join " " (numbers 4 2)))
   (println "Target" (+ 200 (rand-int 800))))
+
+(comment
+
+  (require '[clojure.math.combinatorics :as combo])
+  (mapcat #(combo/combinations [1, 9, 3, 1, 75, 100] %) (range 2 7))
+
+  )
+
+(comment
+  (def cnt 3)
+  (for [len (range 1 (inc cnt))
+        start-index (range cnt)]
+    {:len len :start-index start-index})
+  )
+
+
+(comment
+
+  (def numbers [1 9 3 1 75 100])
+  (def combos (mapcat #(combo/combinations numbers %) (range 2 7)))
+
+  )
--- test/numberly/core_test.clj
+++ test/numberly/core_test.clj
@@ -2,8 +2,22 @@
   (:require [clojure.test :refer :all]
             [numberly.core :refer :all]))
 
-(deftest select-numbers
+(comment deftest select-numbers
   (testing "number selection"
     (let [selected (numbers 4 2)
           _ (println selected)]
     (is (= 6 (count selected))))))
+
+(comment deftest test-sums
+  (testing "finds addends"
+    (is (= '([1 1 2 5 10]) (sums [1 1 2 5 10 20] 19)))
+    (is (= '([1 5 10] [1 5 10]) (sums [1 1 2 5 10 20] 16)))
+    (is (= '() (sums [1 1 2 5 10 20] 40)))))
+
+(deftest test-candidates
+  (testing "single element"
+    (is (= [[25]] (candidates [25]))))
+  (testing "two distinct elements"
+    (is (= [[1] [3] [1 3]] (candidates [1 3]))))
+  (testing "duplicate elements"
+    (is (= [[5] [2] [2 5] [2 5 5]] (candidates [5 2 5])))))
